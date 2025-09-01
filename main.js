797042514d70787544e745e6f2a0262683be43ff
geospachos <george.spachos@icloud.com>
2025-09-01 21:01:34 +0300

    Update main.js (Added cheaper models and resolved issue)

--- main.js
+++ main.js
@@ -39,20 +39,22 @@ const readFile = new tool({
     },
 });
 
-const agent = new Agent({
-  name: "Agent",
+const Summarizer = new Agent({
+  name: "Summarizer",
+  model: "gpt-5-nano-2025-08-07",
   instructions:
     "You get data from the other agent and if the user requests you explain the data to the user",
 });
 
-const agent2 = new Agent({
-  name: "Agent2",
+const DataFetchingAgent = new Agent({
+  name: "DataFetchingAgent",
+  model: "gpt-5-nano-2025-08-07",
   instructions:
     'You use the getData tool to get data from an API or the readFile tool to read the file in the directory. If the data you receive is a JSON, format all elements e.g. "value": 1 you return value is 1, then give it to the other agent agent. If it is just a plain text document, give it directly to the other agent agent',
   tools: [getData, readFile],
-  handoffs: [agent],
+  handoffs: [Summarizer],
 });
 
-const result = await run(agent2, "Read the file using the readFile tool and make a TLDR of it");
+const result = await run(DataFetchingAgent, "Fetch Data and explain it to me"); // Change with what you want the Agent to do
 
 console.log(result.finalOutput);
